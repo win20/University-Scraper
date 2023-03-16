9ac62abe52f1f48250d760445e04331dd209c156
Win Barua <win.barua2@gmail.com>
2023-03-16 00:04:39 +0000

    Retrieve column names

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import time
 import requests
 import random
 
-page = requests.get('http://quotes.toscrape.com/')
-soup = bs(page.content, 'html.parser')
+def get_column_names(table):
+  columnsElements = table.find('thead').find_all('th')
 
-quotes = [i.text for i in soup.find_all(class_ = 'text')]
-authors = [i.text for i in soup.find_all(class_='author')]
-print(authors[0]);
+  columns = []
+  for element in columnsElements:
+    columns.append(element.text)
+
+  columns[-1] = columns[-1].replace('-', '')
+
+  return columns;
+
+def main():
+  page = requests.get('https://www.theguardian.com/education/ng-interactive/2022/sep/24/the-guardian-university-guide-2023-the-rankings')
+  soup = bs(page.content, 'html.parser')
+  table = soup.select('table.c-table')[0]
+
+  columns = get_column_names(table);
+
+  print(columns)
+
+if __name__ == "__main__":
+  main()
