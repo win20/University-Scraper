c67f3bc067e68ba76e3b2fd1bcf7ac72f4d991c4
Win Barua <win.barua@sykescottages.co.uk>
2023-03-16 17:02:39 +0000

    retrieve rows

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ def get_column_names(table):
 
   return columns;
 
+def get_rows(table):
+  rows = []
+  for i, row in enumerate(table.find_all('tr')):
+    if i != 0:
+        rows.append([el.text.strip() for el in row.find_all('td')])
+
+  print(rows[0][0])
+
 def main():
   page = requests.get('https://www.theguardian.com/education/ng-interactive/2022/sep/24/the-guardian-university-guide-2023-the-rankings')
   soup = bs(page.content, 'html.parser')
   table = soup.select('table.c-table')[0]
 
   columns = get_column_names(table);
-
-  print(columns)
+  rows = get_rows(table);
 
 if __name__ == "__main__":
   main()
